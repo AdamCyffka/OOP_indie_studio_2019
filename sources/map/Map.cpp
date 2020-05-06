906288004cb073cb6d9b14e721f92054d1be89cb
Antoine <antoine.robin@epitech.eu>
2020-05-06 17:11:24 +0200

    [+] update gitignore, added boost in cmakelists, map class declaration

---
+++ sources/map/Map.cpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_indie_studio_2019 [WSL: Ubuntu]
+** File description:
+** Map
+*/
+
+#include "map/Map.hpp"
+#include <boost/random.hpp>
+
+Map::Map()
+{
+	generateMap();
+}
+
+Map::~Map() = default;
+
+const std::vector<std::vector<int>> &Map::getMap()
+{
+	return _map;
+}
+
+void Map::generateMap()
+{
+	for (int i = 0; i < MAP_HEIGHT; ++i)
+	{
+		for (int j = 0; j < MAP_WIDTH; ++j)
+		{
+			_map[i][j] = generateBlock();
+		}
+	}
+}
+
+int Map::generateBlock()
+{
+	return -1;
+}
