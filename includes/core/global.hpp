cac80205c10f0ea69eba6a0e59141255dd6769cd
valentin1.masson <valentin1.masson@epitech.eu>
2020-05-13 14:17:10 +0200

    [+] dr_peach and koopa animated md3 models

--- includes/core/global.hpp
+++ includes/core/global.hpp
@@ -30,52 +30,34 @@ extern const std::map<std::string, modelInfos_t> g_modelInfos = {
         }
     },
     {
-        "dry_bones", // STRING
+        "dr_peach", // STRING
         { // MODELINFOS_T
-            "resources/models/characters/dry_bones/dry_bones.MD3", // MODEL MD3
+            "resources/models/characters/dr_peach/dr_peach.MD3", // MODEL MD3
             {
-                "resources/models/characters/dry_bones/Dry_Bones_body.png", // TEXTURES
-                "resources/models/characters/dry_bones/Dry_Bones_eye.png"
+                "resources/models/characters/dr_peach/dr_peach_di.png", // TEXTURES
+                "resources/models/characters/dr_peach/dr.peach_crown_dif.png"
             },
             {0, 120}, // IDLE
-            {121, 159}, // MOVING
-            {160, 298}, // DYING
-            {299, 369}, // DEAD
-            {370, 620}, // VICTORY
+            {121, 140}, // MOVING
+            {141, 279}, // DYING
+            {280, 356}, // DEAD
+            {357, 613}, // VICTORY
             1 // SCALE SIZE
         }
     },
     {
-        "luigi", // STRING
+        "koopa", // STRING
         { // MODELINFOS_T
-            "resources/models/characters/luigi/luigi.MD3", // MODEL MD3
+            "resources/models/characters/koopa/koopa.MD3", // MODEL MD3
             {
-                "resources/models/characters/luigi/Tex_0034_0.png",
-                "resources/models/characters/luigi/Tex_0035_0.png",
-                "resources/models/characters/luigi/Tex_0037_0.png",
-                "resources/models/characters/luigi/Tex_0038_0.png"
+                "resources/models/characters/koopa/koopaBody.png",
+                "resources/models/characters/koopa/koopaFace.png"
             },
             {0, 120}, // IDLE
-            {121, 159}, // MOVING
-            {160, 298}, // DYING
-            {299, 369}, // DEAD
-            {370, 620}, // VICTORY
-            10 // SCALE SIZE
-        }
-    },
-    {
-        "mario", // STRING
-        { // MODELINFOS_T
-            "resources/models/characters/mario/mario.MD3", // MODEL MD3
-            {
-                "resources/models/characters/mario/mario_eye.0.png",
-                "resources/models/characters/mario/mario_all.png"
-            },
-            {0, 120}, // IDLE
-            {121, 159}, // MOVING
-            {160, 298}, // DYING
-            {299, 369}, // DEAD
-            {370, 620}, // VICTORY
+            {121, 142}, // MOVING
+            {143, 281}, // DYING
+            {282, 376}, // DEAD
+            {377, 427}, // VICTORY
             10 // SCALE SIZE
         }
     }
