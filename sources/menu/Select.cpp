02777004856c938b5fe89ad59e9027e2806af9de
valentin1.masson <valentin1.masson@epitech.eu>
2020-05-18 16:17:21 +0200

    [+] changeModel function impl. in Character.cpp

--- sources/menu/Select.cpp
+++ sources/menu/Select.cpp
@@ -16,10 +16,10 @@ Select::Select(irr::gui::IGUIEnvironment *env, irr::video::IVideoDriver *driver,
 
     loadTextures();
     loadButtons();
-    _selectTab = {{"waluigi", true}, {"mario", true}, {"yoshi", true}, {"lakitu", true}, {"dr_peach", false}, {"koopa", false}};
+    _selectTab = {{"waluigi", true}, {"mario", true}, {"lakitu", true}, {"koopa", true}, {"dr_peach", false}};
     _previews.push_back(new Character(_smgr, _driver, g_modelInfos.at(_selectTab[0].first), "preview1"));
     _previews.push_back(new Character(_smgr, _driver, g_modelInfos.at(_selectTab[1].first), "preview2"));
-    _previews.push_back(new Character(_smgr, _driver, g_modelInfos.at(_selectTab[5].first), "preview3"));
+    _previews.push_back(new Character(_smgr, _driver, g_modelInfos.at(_selectTab[2].first), "preview3"));
     _previews.push_back(new Character(_smgr, _driver, g_modelInfos.at(_selectTab[3].first), "preview4"));
     _previews[0]->setPosition({17, 0, 245});
     _previews[1]->setPosition({-28, 0, 225});
