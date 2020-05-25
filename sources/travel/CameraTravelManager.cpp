fb31ca4a0cd669bc9a8da77c0d4ee182e46c3481
Valentin Masson <valentin1.masson@epitech.eu>
2020-05-25 13:54:02 +0200

    [+] gameToScore camera travel

--- sources/travel/CameraTravelManager.cpp
+++ sources/travel/CameraTravelManager.cpp
@@ -47,7 +47,7 @@ void CameraTravelManager::doTravel(travel travel)
                 travel->start();
             } else {
                 _travels[introToMenu] = new StraightCameraTravel(_camera, _smgr,
-                _camera->getPosition(), {-179.2f, 55.8f, -36.1f}, {-111.0f, 45.0f, -24.0f}, u32(6000));
+                _camera->getPosition(), {-179.2f, 55.8f, -36.1f}, {-111.0f, 45.0f, -24.0f}, u32(2000));
                 if (auto travel = getTravel(introToMenu))
                     travel->start();
                 else
@@ -59,7 +59,7 @@ void CameraTravelManager::doTravel(travel travel)
                 travel->start();
             } else {
                 _travels[menuToSelect] = new StraightCameraTravel(_camera, _smgr,
-                {-179.2f, 55.8f, -36.1f}, {-94.3f, 44.1f, 294.3f}, {-70.0f, 21.0f, 232.0f}, u32(6000));
+                {-179.2f, 55.8f, -36.1f}, {-94.3f, 44.1f, 294.3f}, {-70.0f, 21.0f, 232.0f}, u32(2000));
                 if (auto travel = getTravel(menuToSelect))
                     travel->start();
                 else
@@ -71,7 +71,7 @@ void CameraTravelManager::doTravel(travel travel)
                 travel->start();
             } else {
                 _travels[selectToMenu] = new StraightCameraTravel(_camera, _smgr,
-                {-94.3f, 44.1f, 294.3f}, {-179.2f, 55.8f, -36.1f}, {-111.0f, 45.0f, -24.0f}, u32(6000));
+                {-94.3f, 44.1f, 294.3f}, {-179.2f, 55.8f, -36.1f}, {-111.0f, 45.0f, -24.0f}, u32(2000));
                 if (auto travel = getTravel(selectToMenu))
                     travel->start();
                 else
@@ -83,11 +83,23 @@ void CameraTravelManager::doTravel(travel travel)
                 travel->start();
             } else {
                 _travels[selectToGame] = new StraightCameraTravel(_camera, _smgr,
-                    {-94.3f, 44.1f, 294.3f}, {-539.4f, 453.6f, 710.3f}, {-516.3f, 333.2f, 710.5f}, u32(6000));;
+                    {-94.3f, 44.1f, 294.3f}, {-539.4f, 453.6f, 710.3f}, {-516.3f, 333.2f, 710.5f}, u32(2000));;
                 if (auto travel = getTravel(selectToGame))
                     travel->start();
                 else
                     throw CameraTravelException("Camera travel manager can't access to camera travel for: selectToGame");
+            }
+            break;
+        case travel::gameToScore:
+            if (auto travel = getTravel(gameToScore)) {
+                travel->start();
+            } else {
+                _travels[gameToScore] = new StraightCameraTravel(_camera, _smgr,
+                    {-539.4f, 453.6f, 710.3f}, {244.5f, 379.2f, 855.2f}, {230.1f, 358.8f, 920.5f}, u32(2000));;
+                if (auto travel = getTravel(gameToScore))
+                    travel->start();
+                else
+                    throw CameraTravelException("Camera travel manager can't access to camera travel for: gameToScore");
             }
             break;
         default:
