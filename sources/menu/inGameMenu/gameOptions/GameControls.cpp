785640936fed589c29b8ec5eccc9e5eff5329603
Adam <adam.cyffka@epitech.eu>
2020-06-12 15:48:58 +0200

    [+] add number images

--- sources/menu/inGameMenu/gameOptions/GameControls.cpp
+++ sources/menu/inGameMenu/gameOptions/GameControls.cpp
@@ -29,6 +29,24 @@ void GameControls::loadTextures()
     _textures["controls"] = _driver->getTexture("resources/images/buttons/controls.png");
     if (_textures.find("controls") == _textures.end() || !_textures["controls"])
         throw LoadingException("could not load texture : resources/images/buttons/controls.png");
+    _textures["control_keyboard"] = _driver->getTexture("resources/images/buttons/keyboard_control.png");
+	if (_textures.find("control_keyboard") == _textures.end() || !_textures["control_keyboard"])
+		throw LoadingException("could not load texture : resources/images/buttons/keyboard_control.png");
+    _textures["control_controller"] = _driver->getTexture("resources/images/buttons/controller_control.png");
+	if (_textures.find("control_controller") == _textures.end() || !_textures["control_controller"])
+		throw LoadingException("could not load texture : resources/images/buttons/controller_control.png");
+    _textures["p1"] = _driver->getTexture("resources/images/buttons/p1.png");
+	if (_textures.find("p1") == _textures.end() || !_textures["p1"])
+		throw LoadingException("could not load texture : resources/images/buttons/p1.png");
+    _textures["p2"] = _driver->getTexture("resources/images/buttons/p2_2.png");
+	if (_textures.find("p2") == _textures.end() || !_textures["p2"])
+		throw LoadingException("could not load texture : resources/images/buttons/p2_2.png");
+    _textures["p3"] = _driver->getTexture("resources/images/buttons/p3.png");
+	if (_textures.find("p3") == _textures.end() || !_textures["p3"])
+		throw LoadingException("could not load texture : resources/images/buttons/p3.png");
+    _textures["p4"] = _driver->getTexture("resources/images/buttons/p4.png");
+	if (_textures.find("p4") == _textures.end() || !_textures["p4"])
+        throw LoadingException("could not load texture : resources/images/buttons/p4.png");
 }
 
 void GameControls::loadButtons()
@@ -50,6 +68,42 @@ void GameControls::loadButtons()
         throw LoadingException("could not add button : back");
     _buttons["back"]->setImage(_textures["back"]);
     _buttons["back"]->setRelativePosition(irr::core::position2d<irr::s32>(852, 800));
+
+    _images["p1"] = _env->addImage(irr::core::rect<irr::s32>(0, 0, 67, 67));
+	if (_images.find("p1") == _images.end() || !_images["p1"])
+		throw LoadingException("could not add image : p1");
+    _images["p1"]->setImage(_textures["p1"]);
+    _images["p1"]->setRelativePosition(irr::core::position2d<irr::s32>(1300, 340));
+
+    _images["p2"] = _env->addImage(irr::core::rect<irr::s32>(0, 0, 67, 67));
+	if (_images.find("p2") == _images.end() || !_images["p2"])
+		throw LoadingException("could not add image : p2");
+    _images["p2"]->setImage(_textures["p2"]);
+    _images["p2"]->setRelativePosition(irr::core::position2d<irr::s32>(1300, 440));
+
+    _images["p3"] = _env->addImage(irr::core::rect<irr::s32>(0, 0, 67, 67));
+	if (_images.find("p3") == _images.end() || !_images["p3"])
+		throw LoadingException("could not add image : p3");
+    _images["p3"]->setImage(_textures["p3"]);
+    _images["p3"]->setRelativePosition(irr::core::position2d<irr::s32>(1300, 540));
+
+    _images["p4"] = _env->addImage(irr::core::rect<irr::s32>(0, 0, 67, 67));
+	if (_images.find("p4") == _images.end() || !_images["p4"])
+		throw LoadingException("could not add image : p4");
+    _images["p4"]->setImage(_textures["p4"]);
+    _images["p4"]->setRelativePosition(irr::core::position2d<irr::s32>(1300, 640));
+
+    _images["control_keyboard"] = _env->addImage(irr::core::rect<irr::s32>(0, 0, 200, 401));
+	if (_images.find("control_keyboard") == _images.end() || !_images["control_keyboard"])
+		throw LoadingException("could not add image : control_keyboard");
+    _images["control_keyboard"]->setImage(_textures["control_keyboard"]);
+    _images["control_keyboard"]->setRelativePosition(irr::core::position2d<irr::s32>(1400, 320));
+
+    _images["control_controller"] = _env->addImage(irr::core::rect<irr::s32>(0, 0, 199, 82));
+	if (_images.find("control_controller") == _images.end() || !_images["control_controller"])
+		throw LoadingException("could not add image : control_controller");
+    _images["control_controller"]->setImage(_textures["control_controller"]);
+    _images["control_controller"]->setRelativePosition(irr::core::position2d<irr::s32>(1400, 230));
 }
 
 std::vector<Character *> GameControls::getPreviews()
